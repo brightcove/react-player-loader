f25992a31bb7640e8addacd3d5c667d94599c15d
brandonocasey <brandonocasey@gmail.com>
2018-09-05 11:31:41 -0400

    fix: Remove the postinstall script to prevent install issues

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -1,11 +1,22 @@
 const generate = require('videojs-generate-karma-config');
 
 module.exports = function(config) {
-  config = generate(config);
 
-  config.files = [
-    'node_modules/react/umd/react.development.js',
-    'node_modules/react-dom/umd/react-dom.development.js',
-    'test/dist/bundle.js'
-  ];
+  // see https://github.com/videojs/videojs-generate-karma-config
+  // for options
+  const options = {
+    files(defaults) {
+      // defaults don't work for this project
+      return [
+        'node_modules/react/umd/react.development.js',
+        'node_modules/react-dom/umd/react-dom.development.js',
+        'test/dist/bundle.js'
+      ];
+    }
+  };
+
+  config = generate(config, options);
+
+  // any other custom stuff not supported by options here!
 };
+
